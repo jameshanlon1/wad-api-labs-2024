e7cf3177aebc00640cf2aae5d7804adbe91b9bb1
james <jameshanlonrcs@gmail.com>
2024-11-25 18:20:55 +0000

    Mongo lab Users Router

--- tasky-api/api/tasks/taskModel.js
+++ tasky-api/api/tasks/taskModel.js
@@ -7,7 +7,11 @@ const TaskSchema = new Schema({
   deadline: Date,
     done: Boolean,
     priority: { type: String, enum: ["Low", "Medium", "High"], required: true },    created_at: Date,
-    updated_at: Date
+    updated_at: Date,
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
 });
 const dateValidator = (date) => {
     return date > new Date();
--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -22,6 +23,9 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
+
+//Users router
+app.use('/api/users', usersRouter);
 
 app.use(errHandler);
 
